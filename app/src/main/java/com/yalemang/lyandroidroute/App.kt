1411062be0d64ec2664079e9432051d923ec29dd
陈浪 <1961075191@qq.com>
2023-04-05 15:48:44 +0800

    Update 2023.04.05

--- app/src/main/java/com/yalemang/lyandroidroute/App.kt
+++ app/src/main/java/com/yalemang/lyandroidroute/App.kt
@@ -1,6 +1,14 @@
 package com.yalemang.lyandroidroute
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
 }
--- app/src/main/java/com/yalemang/lyandroidroute/MainActivity.kt
+++ app/src/main/java/com/yalemang/lyandroidroute/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.yalemang.lyandroidroute
 
-import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
